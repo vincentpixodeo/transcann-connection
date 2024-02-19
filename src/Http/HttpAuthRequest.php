39e90d0c67c7ab44d9a86b66a3380be78e932390
Vincent <vincent@pixodeo.net>
2024-02-19 14:36:14 +0700

    no message

--- src/Http/HttpAuthRequest.php
+++ src/Http/HttpAuthRequest.php
@@ -20,21 +20,12 @@ class HttpAuthRequest extends AbstractRequestAction implements HttpRequestInterf
     public function __construct(ClientInterface $client = null)
     {
         $this->authentication = WmsXtentService::authentication();
-        parent::__construct($this->authentication->getClient());
-    }
 
-    /**
-     * @throws \Exception
-     */
-    public function getClient(): ClientInterface
-    {
+        parent::__construct($this->authentication->getClient());
+
         $token = $this->authentication->getToken();
 
-        if (empty($token)) {
-            throw new \Exception('The token is empty');
-        }
         $this->_client->setToken($token);
-        return $this->_client;
     }
 
     function getCurrentLog(): ?Log
